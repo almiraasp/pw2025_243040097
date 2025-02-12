4b14964b975e9d0ca8cf6cc5186e73497b03c33f
almiraasp <almirap430@gmail.com>
2025-02-12 21:28:07 +0700

    Mengerjakan Latihan di pertemuan kedua

---
+++ kuliah/pertemuan2/latihan2.php
@@ -0,0 +1,12 @@
+<?php
+$nama = "Almira Alifia Suparya Putri";
+$email = "Almirap430@gmail.com";
+$pekerjaan = "Mahasiswa";
+$jurusan = "Teknik Informatika";
+$universitas = "Universitas Pasundan";
+
+    echo "<h1>$nama</h1>";
+    echo "<p>$email</p>";
+    echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan di $jurusan $universitas"
+
+?>
