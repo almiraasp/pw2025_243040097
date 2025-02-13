449e13305fe2a547c49eac5be3ad1ec4c7527406
almiraasp <almirap430@gmail.com>
2025-02-13 20:37:59 +0700

    Mengerjakan contoh program penggunaan variabel, tipe data variabel, operator, dan konstanta

---
+++ kuliah/pertemuan2/variabel_TipeData.php
@@ -0,0 +1,14 @@
+<?php
+
+$nrp = "243040097";
+$nama = "Almira Alifia Suparya Putri";
+$umur = 19;
+$nilai = 88.23;
+
+echo "NRP   : ". $nrp . "</br>";
+echo "Nama  : $nama</br>";
+print "Umur : ".$umur; print "</br>";
+// menampilkan bilangan real dengan format 3 digit dibelakang koma
+printf ("Nilai  : %.3f</br>", $nilai); 
+
+?>
