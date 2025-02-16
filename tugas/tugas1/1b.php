f1ba8e5bd05ba5974e3e1e8afe09f46a0ff911f0
almiraasp <almirap430@gmail.com>
2025-02-16 20:56:37 +0700

    Menyelesaikan tugas 1

---
+++ tugas/tugas1/1b.php
@@ -0,0 +1,19 @@
+<?php
+
+$npm = 97;
+
+echo "Aku adalah angka <b>" . $npm . "</b></br>";
+
+$kali = $npm * 5;
+echo "Jika aku dikali 5, maka aku sekarang menjadi <b>" . $kali . "</b></br>";
+
+$bagi = $npm / 2;
+echo "Jika aku dibagi 2, maka aku sekarang menjadi <b>" . $bagi . "</b></br>";
+
+$tambah = $npm + 75;
+echo "Jika aku ditambah 75, maka aku sekarang menjadi <b>" . $tambah . "</b></br>";
+
+$kurang = $npm - 20;
+echo "Jika aku dikurang 20, maka aku sekarang menjadi <b>" . $kurang . "</b></br>";
+
+?>
---
+++ tugas/tugas1/1c.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tugas 1c</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="box">1</div>
+        </div>
+
+        <div class="row">
+            <div class="box">2</div>
+            <div class="box">2</div>
+        </div>
+        
+        <div class="row">
+            <div class="box">3</div>
+            <div class="box">3</div>
+            <div class="box">3</div>
+        </div>
+    </div>
+</body>
+</html>
