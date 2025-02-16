e212191bb541f6f4f493b47f006170334423a734
almiraasp <almirap430@gmail.com>
2025-02-16 21:03:17 +0700

    Menambahkan folder css

--- tugas/tugas1/1c.php
+++ tugas/tugas1/1c.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>Tugas 1c</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
     <div class="container">
@@ -14,7 +14,7 @@
             <div class="box">2</div>
             <div class="box">2</div>
         </div>
-        
+
         <div class="row">
             <div class="box">3</div>
             <div class="box">3</div>
