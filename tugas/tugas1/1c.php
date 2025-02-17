b2a7c6cdfbe6b14315e293dd653c021a089274c0
almiraasp <almirap430@gmail.com>
2025-02-17 11:58:34 +0700

    Mengerjakan tugas 1c

--- tugas/tugas1/1c.php
+++ tugas/tugas1/1c.php
@@ -1,25 +1,30 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Tugas 1c</title>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 1C</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="box">1</div>
-        </div>
 
-        <div class="row">
-            <div class="box">2</div>
-            <div class="box">2</div>
-        </div>
-
-        <div class="row">
-            <div class="box">3</div>
-            <div class="box">3</div>
-            <div class="box">3</div>
-        </div>
+<div class="container">
+    <div class="row">
+        <div class="kotak">1</div>
     </div>
+
+    <div class="row">
+        <div class="kotak">2</div>
+        <div class="kotak">2</div>
+    </div>
+
+    <div class="row">
+        <div class="kotak">3</div>
+        <div class="kotak">3</div>
+        <div class="kotak">3</div>
+    </div>
+    
+</div>
+    
 </body>
 </html>
